8d97f8eb9b281f024a8782441c0fb504c23bb34c
colemcginn <colemcginn@hotmail.com>
2017-10-18 22:02:23 -0700

    saving minecraft progress

---
+++ TestPluginCommandExecutor.java
@@ -0,0 +1,60 @@
+package io.github.colemcginn.TestPlugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.entity.Player;
+
+public class TestPluginCommandExecutor implements CommandExecutor {
+	private final TestPlugin plugin;
+
+	public TestPluginCommandExecutor(TestPlugin plugin) {
+		this.plugin = plugin; // Store the plugin in situations where you need it.
+	}
+	
+	@Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(cmd.getName().equalsIgnoreCase("testplugin")) {
+			if(args.length==1) {
+				if(args[0].equalsIgnoreCase("reload")) {
+					//
+					plugin.reloadConfig();
+					return true;
+				}
+			}
+			if(args.length>1) {
+				//
+				if(args[0].equalsIgnoreCase("join")) {
+					String msg = new String();
+					for(int i=1;i<args.length;i++) {
+						msg.concat(args[i]);
+					}
+					plugin.getConfig().set("join",msg);
+					plugin.saveConfig();
+					FileConfiguration config = plugin.getConfig();
+					String newmsg = config.getString("join");
+//					sender.getPlayer().sendMessage(msg);
+					Bukkit.broadcastMessage(newmsg);
+					//I chose not to reload because of /reload command
+				}
+				return true;
+			} 
+		}
+//    	if (cmd.getName().equalsIgnoreCase("basic")) { // If the player typed /basic then do the following...
+//    		// do something...
+//    		sender.sendMessage("Hello basic");
+//    		return true;
+//    	} else if (cmd.getName().equalsIgnoreCase("basic2")) {
+//    		if (!(sender instanceof Player)) {
+//    			sender.sendMessage("This command can only be run by a player.");
+//    		} else {
+//    			Player player = (Player) sender;
+//    			// do something
+//    		}
+//    		return true;
+//    	}
+    	return false;
+    }
+}
---
+++ TestPluginConfigListener.java
@@ -0,0 +1,45 @@
+package io.github.colemcginn.TestPlugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class TestPluginConfigListener implements Listener{
+		TestPlugin plugin;
+		 
+		public TestPluginConfigListener(TestPlugin instance) {
+		plugin = instance;
+		}
+		 
+		@EventHandler
+		public void onJoin(PlayerJoinEvent event) {
+//		Player player = e.getPlayer();
+//		 
+//		player.sendMessage(plugin.getConfig().getString("msg1"));
+			FileConfiguration config = this.plugin.getConfig();
+			String msg = config.getString("join");
+			
+			OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(event.getPlayer().getName());
+			if(!offlinePlayer.hasPlayedBefore()){
+				msg = config.getString("joinfirst");
+			}
+			event.getPlayer().sendMessage(msg);
+		}
+		
+		@EventHandler
+		public void onBlockBreak(BlockBreakEvent event) {
+			FileConfiguration config = plugin.getConfig();
+			Boolean replace = config.getBoolean("replaceblock");
+			if(replace) {
+				event.setCancelled(true);
+				event.getBlock().setType(Material.ICE);
+			}
+		}
+		
+}
